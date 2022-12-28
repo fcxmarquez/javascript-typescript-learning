cff9e2ee09346ce1a758e5447f5608960cda6977
Francisco Márquez <francisco.marquez.solt@gmail.com>
2022-12-28 21:58:08 +0000

    What is a pointer

--- src/javascript/algorithms and data structures/big O/dataStructures/linkList/1linkList.js
+++ src/javascript/algorithms and data structures/big O/dataStructures/linkList/1linkList.js
@@ -1,4 +1,4 @@
-const basket = ['apples', 'grapes', 'pears'];
+const basket = ["apples", "grapes", "pears"];
 
 // linked list: apples --> grapes --> pears The arrows are pointers
 
@@ -7,3 +7,15 @@ const basket = ['apples', 'grapes', 'pears'];
 //          8742 --> pears
 //                   372 --> null
 // The numbers are the memory address of the next item in the list
+
+let obj1 = { a: true };
+let obj2 = obj1;
+// This work like a pointer because it's a reference. You reference the obj2 to obj1 instead of create a new space memory.
+obj1.a = "hello";
+// The obj2 also change because it's a reference.
+delete obj1; // This delete the obj1 but not the obj2 because it's a reference. So deleting obj1 still have in memory the obj2 because there is a reference pointting to the obj deleted. Thanks to the garbage collector. In low level languages you have to manually handle the memory.
+
+// console.log('1', obj1);
+console.log("2", obj2);
+
+
