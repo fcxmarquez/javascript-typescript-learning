ebed2112c6de3de4e9a46a466844ee311be91e08
Francisco Márquez <francisco.marquez.solt@gmail.com>
2022-12-28 01:36:26 +0000

    Linked list

--- src/javascript/algorithms and data structures/big O/dataStructures/hashTables/2ImplementingAHastable.js
+++ src/javascript/algorithms and data structures/big O/dataStructures/hashTables/2ImplementingAHastable.js
@@ -34,25 +34,36 @@ class HashTable {
     }
     return currentBucket[0][1];
   }
+
+  keys() {
+    const keysArray = [];
+
+    for (let i = 0; i < this.data.length; i++) {
+      const currentBucket = this.data[i];
+      if (currentBucket && currentBucket.length > 1) {
+        for (let j = 0; j < currentBucket.length; j++) {
+          const element = currentBucket[j];
+          keysArray.push(element[0]);
+        }
+        break;
+      }
+
+      if (currentBucket) {
+        keysArray.push(currentBucket[0][0]);
+      }
+    }
+
+    return keysArray;
+  }
 } //O(1) - constant time but O(n) - linear time if there is a collision
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-console.log(myHashTable.get("grapes"));
 myHashTable.set("apples", 9);
-console.log(myHashTable.get("apples"));
+myHashTable.set("oranges", 2);
 
-// --------
-class HashTableAndrei {
-  constructor(size) {
-    this.data = new Array(size);
-  }
+// console.log(myHashTable.get("grapes"));
+// console.log(myHashTable.get("apples"));
+// console.log(myHashTable.get("oranges"));
 
-  _hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
-    }
-    return hash;
-  } // The undesrscore is a convention to indicate that this is a private method. In javascript there is no way to make a method private.
-}
+console.log(myHashTable.keys());
---
+++ src/javascript/algorithms and data structures/big O/dataStructures/linkList/1linkList.js
@@ -0,0 +1,9 @@
+const basket = ['apples', 'grapes', 'pears'];
+
+// linked list: apples --> grapes --> pears The arrows are pointers
+
+// apples
+// 8947 --> grapes
+//          8742 --> pears
+//                   372 --> null
+// The numbers are the memory address of the next item in the list
